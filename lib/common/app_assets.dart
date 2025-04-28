53ced1530f19ac58082dd31ecbe46f6255c32fb3
Mostafa Samir <mostafasamir.3338@gmail.com>
2025-04-28 11:47:53 +0300

    native splash screen done

--- lib/common/app_assets.dart
+++ lib/common/app_assets.dart
@@ -1,3 +1,6 @@
-class AppImages{
-
+class AppImages {
+  static const String splashScreenLogo =
+      'assets/images/splash_screen/splash_screen_logo.png';
+  static const String splashScreenBranding =
+      'assets/images/splash_screen/splash_screen_branding.png';
 }
