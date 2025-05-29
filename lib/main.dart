4b3536e36d68152118aa61c8007aeb7808f7a7bd
Mostafa Samir <mostafasamir.3338@gmail.com>
2025-05-30 02:51:47 +0300

    caching theme and localization by shared preference

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,15 @@ import 'package:evently/onboarding/screens/onboarding_screen_1.dart';
 import 'package:evently/onboarding/screens/onboarding_screen_2.dart';
 import 'package:evently/providers/create_event_screen_provider.dart';
 import 'package:evently/providers/localization_provider.dart';
-import 'package:evently/providers/map_tab_provider.dart';
 import 'package:evently/providers/theme_provider.dart';
 import 'package:evently/providers/user_auth_provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'events/screens/event_details_screen.dart';
 import 'events/screens/pick_location_screen_for_edit_event.dart';
@@ -34,13 +35,19 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await AppPrefs.init();
+  //caching localization(shared preference)
+  final localizationProvider = LocalizationProvider();
+  await localizationProvider.localizationGetBool();
+  //caching theme(shared preference)
+  final themeProvider = ThemeProvider();
+  await themeProvider.themeGetBool();
   runApp(MultiProvider(providers: [
-    ChangeNotifierProvider<ThemeProvider>(
-        create: (BuildContext context) => ThemeProvider()),
+    ChangeNotifierProvider<ThemeProvider>.value(
+        value: themeProvider),
     ChangeNotifierProvider<UserAuthProvider>(
         create: (BuildContext context) => UserAuthProvider()),
-    ChangeNotifierProvider<LocalizationProvider>(
-        create: (BuildContext context) => LocalizationProvider()),
+    ChangeNotifierProvider<LocalizationProvider>.value(
+        value: localizationProvider),
     ChangeNotifierProvider<CreateEventScreenProvider>(
         create: (BuildContext context) => CreateEventScreenProvider()),
   ], child: const MyApp()));
@@ -52,6 +59,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
       localizationsDelegates: [
         AppLocalizations.delegate,
@@ -87,11 +96,9 @@ class MyApp extends StatelessWidget {
           );
         },
         PickLocationScreenForEditEvent.routeName: (context) {
-          Map arguments = ModalRoute.of(context)!
-              .settings
-              .arguments as Map;
-          CreateEventScreenProvider provider=arguments['provider'];
-          EventDataModel eventDataModel=arguments['eventDataModel'];
+          Map arguments = ModalRoute.of(context)!.settings.arguments as Map;
+          CreateEventScreenProvider provider = arguments['provider'];
+          EventDataModel eventDataModel = arguments['eventDataModel'];
           return PickLocationScreenForEditEvent(
             provider: provider,
             eventDataModel: eventDataModel,
--- lib/providers/localization_provider.dart
+++ lib/providers/localization_provider.dart
@@ -1,19 +1,34 @@
+import 'package:evently/common/app_constants.dart';
+import 'package:evently/common/app_prefs.dart';
 import 'package:flutter/cupertino.dart';
 
 class LocalizationProvider extends ChangeNotifier {
   String appLocalization = 'en';
 
-  changeLocalization() {
+  Future<void> changeLocalization() async {
     if (appLocalization == 'en') {
       appLocalization = 'ar';
     } else {
       appLocalization = 'en';
     }
+    //caching localization(shared preference)
+    await AppPrefs.localizationSetBool(
+        AppConstants.localizationKey, appLocalization == 'en' ? true : false);
     notifyListeners();
   }
 
-String getAppLocaleString(){
-    return appLocalization=='en'?'ar':'en';
-}
+  String getAppLocaleString() {
+    return appLocalization == 'en' ? 'ar' : 'en';
+  }
 
+  //caching localization(shared preference)
+  Future<void> localizationGetBool() async {
+    bool? localizationGetBool =
+       AppPrefs.localizationGetBool(AppConstants.localizationKey);
+    if (localizationGetBool == true) {
+      appLocalization = 'en';
+    } else {
+      appLocalization = 'ar';
+    }
+  }
 }
